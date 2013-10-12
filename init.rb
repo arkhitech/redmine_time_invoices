33dced198c8f4d878804f67a98e3ae2b610f0ca0
Babar Rehman <babar.rehman@arkhitech.com>
2013-10-12 19:38:08 +0500

    Initialization fixed

--- init.rb
+++ init.rb
@@ -1,11 +1,13 @@
-Redmine::Plugin.register :invoice_generator do
-  name 'Invoice Generator plugin'
-  author 'Saba'
-  description 'This is a plugin for Redmine'
+Redmine::Plugin.register :redmine_time_invoices do
+  name 'Redmine Time Invoices'
+  author 'Arkhitech'
+  description 'Generates time invoice for each project where the Redmine Time Invoices plugin module is installed'
   version '0.0.1'
    
   project_module :time_invoices do
-    permission :view_time_invoices, :time_invoices => :index
-menu :project_menu, :time_invoices, { :controller => 'time_invoices', :action => 'index' }, :caption => 'time_invoices', :after => :activity, :param => :project_id
-end
+    permission :submit_invoiceable_time,:time_invoices => :index
+    menu :project_menu, :time_invoices, { :controller => 'time_invoices', :action => 'index' }, :caption => 'time_invoices', :after => :activity, :param => :project_id
+    menu :top_menu, :time_invoices, { :controller => 'time_invoices', :action => 'indexall' }, :caption => 'time_invoices'  
+  end
+  settings default: {'mail' => 'info@arkhitech.com'}, partial: 'settings/invoice_settings'
 end
