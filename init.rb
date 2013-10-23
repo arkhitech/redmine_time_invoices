57e42ffa55b5ae031a6252fe91c81a7c682511f6
Saba Ashraf <saba.ashraf@arkhitech.com>
2013-10-23 18:52:19 +0500

    views refactored, access restricted

--- init.rb
+++ init.rb
@@ -8,9 +8,16 @@ Redmine::Plugin.register :redmine_time_invoices do
    
   project_module :time_invoices do
     permission :submit_invoiceable_time,:time_invoices => :index
-    menu :project_menu, :time_invoices, { :controller => 'time_invoices', :action => 'index' }, :caption => 'time_invoices', :after => :activity, :param => :project_id
-    menu :top_menu, :time_invoices, { :controller => 'time_invoices', :action => 'indexall' }, :caption => 'time_invoices',  
-  if: Proc.new {User.current.allowed_to_globally?(:submit_invoiceable_time,{})}
+    
+    menu :project_menu, :time_invoices, 
+      { controller: 'time_invoices', action: 'index' }, 
+      caption: 'time_invoices', after: :activity, param: :project_id,
+      if: Proc.new {User.current.allowed_to?(:submit_invoiceable_time, Project.find(params[:project_id]))}
+    
+    menu :top_menu, :time_invoices, 
+      { controller: 'time_invoices', action: 'indexall' }, 
+      caption: 'time_invoices', if: Proc.new {User.current.allowed_to_globally?(:submit_invoiceable_time,{})}
+  
   end
   settings default: {'mail' => 'info@arkhitech.com'}, partial: 'settings/invoice_settings'
 end
